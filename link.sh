ade8924087bc4a80fd415644e7d6a0ab42ef58aa
Sosuke Suzuki <aosukeke@gmail.com>
2018-06-24 04:30:11 +0900

    vimrc => init.vim

--- link.sh
+++ link.sh
@@ -2,8 +2,5 @@
 
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
-ln -sf ${SCRIPT_DIR}/.vimrc ~/.vimrc
-ln -sf ${SCRIPT_DIR}/.vim/config ~/.vim/config
-
-ln -sf ${SCRIPT_DIR}/.vimrc ~/.config/nvim/init.vim
+ln -sf ${SCRIPT_DIR}/init.vim ~/.config/nvim/init.vim
 ln -sf ${SCRIPT_DIR}/.vim/config ~/.config/nvim
