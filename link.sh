119761622c7d5348c618486d8e610dcf223f5d90
Sosuke Suzuki <aosukeke@gmail.com>
2019-08-02 19:04:03 +0900

    Fix dirname .vim => nvim

--- link.sh
+++ link.sh
@@ -3,4 +3,4 @@
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
 ln -sf ${SCRIPT_DIR}/init.vim ~/.config/nvim/init.vim
-ln -sf ${SCRIPT_DIR}/.vim/config ~/.config/nvim
+ln -sf ${SCRIPT_DIR}/nvim/config ~/.config/nvim
